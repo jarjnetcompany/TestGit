bede5bd63e13483bf2c1f473d1cead33d48d201c
jarjnetcompany <jarjnetcompany@gmail.com>
2018-03-20 14:02:28 +0100

    Main java class was added

---
+++ MainClass.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hi, this is just a dummy example run at  " + new Date());
+    }
+}
+
+
